4adcd673f58d525562f9f314bc7bfcc46036f8f5
Akhil <akhilvijayan004@gmail.com>
2020-01-26 11:31:38 +1100

    update mysqlconnection

--- config/database.php
+++ config/database.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Str;
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 
 return [
 
@@ -15,7 +21,9 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'your_heroku_mysql_connection'), 
+
+
 
     /*
     |--------------------------------------------------------------------------
@@ -42,6 +50,17 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
+        
+        'your_heroku_mysql_connection' => array(
+            'driver' => 'mysql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+        ),
 
         'mysql' => [
             'driver' => 'mysql',
